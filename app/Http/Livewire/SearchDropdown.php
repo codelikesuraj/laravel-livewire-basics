919451fd5e678b8da88c44bc46a5539e70582469
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-09-02 19:48:25 +0100

    added dropdown search bar

--- app/Http/Livewire/SearchDropdown.php
+++ app/Http/Livewire/SearchDropdown.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchDropdown extends Component
 {
+    public $search;
+    public $searchResults = [];
+
     public function render()
     {
+        $response = Http::get('https://itunes.apple.com/search/?term='.''.'&limit=10');
+        // dd($response->json());
+
         return view('livewire.search-dropdown');
+    }
+
+    public function updatedSearch()
+    {
+        $response = Http::get('https://itunes.apple.com/search/?term='.$this->search.'&limit=10');
+        $this->searchResults = $response['results'];
     }
 }
--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,4 +1,4 @@
-<div class="px-5 my-5 text-sm gap-3 m-auto w-full lg:w-2/3 min-h-screen">
+<div class="px-5 my-5 text-sm gap-3 m-auto w-full lg:w-4/5 min-h-screen">
     <h2 class="text-2xl font-semibold p-2 pl-0 m-4 ml-0">Livewire Search Dropdown</h2>
     <div class="p-0 w-full shadow-sm shadow-slate-200">
         <div class="grid grid-cols-2 justify-between">
@@ -11,13 +11,36 @@
                     <li class="py-5 border-b-2 hover:border-b-indigo-500 border-b-transparent">Calendar</li>
                 </ul>
             </div>
-            <div class="flex flex-col absolute justify-self-end items-center py-3 pr-2 text-sm space-y-2">
+            <div class="grid grid-cols-1 justify-self-end items-center py-3 pr-2 text-sm space-y-2">
                 <label>
-                    <input class="rounded-sm border-2 form-input p-1 border-gray-200" type="text" placeholder="Search for songs..." />
+                    <input wire:model.debounce.300ms="search"
+                        class="rounded-sm border-2 w-64 sm:w-72 form-input p-1 border-gray-200" type="text"
+                        placeholder="Search for songs..." />
                 </label>
-                <ul class="bg-white w-full rounded-sm  border-gray-200 border-2">
-                    <li class="p-2">Hello</li>
-                </ul>
+                @if (strlen(trim($search)) > 0)
+                    <div class="relative">
+                        <ul class="absolute bg-white w-72 grid grid-cols-1 rounded-sm max-w-full border-gray-200 border-2 max-h-[50vh] overflow-y-scroll"
+                            wire:model="searchResults">
+                            @forelse($searchResults as $result)
+                                <li class="p-2 inline-flex justify-start items-center hover:bg-slate-300">
+                                    <div class="">
+                                        <img src="{{ $result['artworkUrl100'] }}"
+                                            class="w-[100px] h-[100px] object-contain" />
+                                    </div>
+                                    <div class="ml-2 w-full font-semibold">
+                                        <p class="max-w-fit font-bold text-indigo-500">
+                                            {{ $result['artistName'] ?? '---' }}</p>
+                                        <p class="max-w-fit text-gray-800">{{ $result['trackName'] ?? '---' }}</p>
+                                    </div>
+                                </li>
+                            @empty
+                                <li class="p-2 inline-flex justify-center">
+                                    "No results for {{ $search }}"
+                                </li>
+                            @endforelse
+                        </ul>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
